266336ce4544d5e05bd84fd66b659b5826f1f246
Peter Morris <mrpmorris@gmail.com>
2020-09-06 16:24:59 +0100

    Title scene

--- source/ThePeriodicTableOfElementsGame/Scenes/MainMenu.razor.cs
+++ source/ThePeriodicTableOfElementsGame/Scenes/MainMenu.razor.cs
@@ -1,6 +1,15 @@
-﻿namespace ThePeriodicTableOfElementsGame.Scenes
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ThePeriodicTableOfElementsGame.Scenes
 {
 	public partial class MainMenu
 	{
+		[Inject]
+		private NavigationManager NavigationManager { get; set; }
+
+		private void PlayMatchGame()
+		{
+			NavigationManager.NavigateTo("/match-game");
+		}
 	}
 }
